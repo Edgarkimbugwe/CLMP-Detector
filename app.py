db60c8090e004d0c1d0b4b7142a82137df7ca5e6
Edgarkimbugwe <kimbuny@gmail.com>
2024-09-23 20:13:37 +0000

    added app initialization and project summary page for Cherry Powdery Mildew detector

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from app_pages.multipage import MultiPage
+
+# Import the functions that generate the content for corresponding pages
+from app_pages.page_summary import page_summary_body
+
+
+# Create an instance of the MultiPage app with the title "CLMP Detector"
+app = MultiPage(app_name="CLMP Detector")
+
+
+# Add pages to the app, associating them with their corresponding functions
+app.add_page("Quick Project Summary", page_summary_body)
+
+
+app.run()  # Run the app
