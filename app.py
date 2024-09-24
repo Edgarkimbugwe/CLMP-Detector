aa69c93db54786d9cd4646907d500dc79010a87f
Edgarkimbugwe <kimbuny@gmail.com>
2024-09-24 17:01:06 +0000

    added leaves visualizer page to the app that will generate the function that will be created in the body

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from app_pages.multipage import MultiPage
 
 # Import the functions that generate the content for corresponding pages
 from app_pages.page_summary import page_summary_body
+from app_pages.leaves_visualizer import leaves_visualizer_body
 
 
 # Create an instance of the MultiPage app with the title "CLMP Detector"
@@ -11,6 +12,7 @@ app = MultiPage(app_name="CLMP Detector")
 
 # Add pages to the app, associating them with their corresponding functions
 app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Leaves Visualiser", leaves_visualizer_body)
 
 
 app.run()  # Run the app
