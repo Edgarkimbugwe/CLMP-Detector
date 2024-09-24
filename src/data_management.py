789a8b8488df3ba207fd3e5557f69571734c8067
Edgarkimbugwe <kimbuny@gmail.com>
2024-09-24 22:07:13 +0000

    added function to load objects from pickle files using Joblib

--- src/data_management.py
+++ src/data_management.py
@@ -16,3 +16,6 @@ def download_dataframe_as_csv(df):
         f'target="_blank">Download Report</a>'
     )
     return href
+
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
