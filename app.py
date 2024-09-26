20aa42fc32e9717d376f289329dd0c4494a6bc49
Edgarkimbugwe <kimbuny@gmail.com>
2024-09-26 21:44:24 +0000

    added 'project_hypothesis' page to the app.py file that will generate the function that will be created in the body

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from app_pages.page_summary import page_summary_body
 from app_pages.leaves_visualizer import leaves_visualizer_body
 from app_pages.ml_performance import ml_performance_metrics
 from app_pages.powdery_mildew_detector import powdery_mildew_detector_body
+from app_pages.project_hypothesis import project_hypothesis_body
+
+
 
 
 # Create an instance of the MultiPage app with the title "CLMP Detector"
@@ -17,6 +20,7 @@ app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Leaves Visualiser", leaves_visualizer_body)
 app.add_page("ML Performance Metrics", ml_performance_metrics)
 app.add_page("Powdery Mildew detector", powdery_mildew_detector_body)
+app.add_page("Project Hypothesis", project_hypothesis_body)
 
 
 app.run()  # Run the app
