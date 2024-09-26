425d855e0b3da0256ce7668e2f0215b777c26a64
Edgarkimbugwe <kimbuny@gmail.com>
2024-09-26 20:31:57 +0000

    added 'ml performance' page to the app.py file that will generate the function that will be created in the body

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from app_pages.multipage import MultiPage
 # Import the functions that generate the content for corresponding pages
 from app_pages.page_summary import page_summary_body
 from app_pages.leaves_visualizer import leaves_visualizer_body
+from app_pages.ml_performance import ml_performance_metrics
 from app_pages.powdery_mildew_detector import powdery_mildew_detector_body
 
 
@@ -14,6 +15,7 @@ app = MultiPage(app_name="CLMP Detector")
 # Add pages to the app, associating them with their corresponding functions
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Leaves Visualiser", leaves_visualizer_body)
+app.add_page("ML Performance Metrics", ml_performance_metrics)
 app.add_page("Powdery Mildew detector", powdery_mildew_detector_body)
 
 
