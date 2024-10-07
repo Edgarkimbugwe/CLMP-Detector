46eca75423d8efc00a6a6be15a0bde5faa102a7e
Edgarkimbugwe <kimbuny@gmail.com>
2024-10-07 11:47:01 +0000

    fix PEP errors in the ml_perfomance.py page

--- app_pages/ml_performance.py
+++ app_pages/ml_performance.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.image import imread
 
+
 def ml_performance_metrics():
     st.write("---")
-    
+
     version = 'v1'
-    
+
     st.info(
-        f"This page provides a detailed overview of how the dataset was divided, how the model performed on that data, and an explanation of the results."
+        "This page provides a detailed overview of how the dataset was divided, "
+        "how the model performed on that data, and an explanation of the results."
     )
 
     st.write("\n")
@@ -19,52 +21,62 @@ def ml_performance_metrics():
     st.write("\n")
 
     st.warning(
-        f"The dataset was divided into three sets:\n\n"
-        f"- **Train set** (70%): Used to train the model by learning general patterns and making predictions.\n\n"
-        f"- **Validation set** (10%): Helps tune the model's parameters after each epoch.\n\n"
-        f"- **Test set** (20%): Used to evaluate the final performance of the model on new, unseen data."
+        "The dataset was divided into three sets:\n\n"
+        "- **Train set** (70%): Used to train the model by learning general patterns "
+        "and making predictions.\n\n"
+        "- **Validation set** (10%): Helps tune the model's parameters after each epoch.\n\n"
+        "- **Test set** (20%): Used to evaluate the final performance of the model "
+        "on new, unseen data."
     )
-    
+
     st.write("\n")
-    
+
     labels_distribution = plt.imread(f"outputs/{version}/leaves_dataset.png")
     st.image(labels_distribution, caption='Dataset Labels Distribution')
 
     st.write("\n")
 
-    labels_distribution_pie = plt.imread(f"outputs/{version}/sets_distribution_pie.png")
-    st.image(labels_distribution_pie, caption='Dataset Distribution Across Sets on a Pie Chart')
+    labels_distribution_pie = plt.imread(
+        f"outputs/{version}/sets_distribution_pie.png"
+    )
+    st.image(labels_distribution_pie, caption='Dataset Distribution Across Sets '
+                                                'on a Pie Chart')
 
-       
     st.write("---")
 
     st.write("### Model Performance Metrics")
 
     st.write("\n")
-    
+
     model_clf_report = plt.imread(f"outputs/{version}/clf_report.png")
     st.image(model_clf_report, caption='Classification Report')
 
     st.write("\n")
 
     st.warning(
-        f"**Classification Report**\n\n"
-        f"- **Precision**: Measures the percentage of true positives out of all predicted positives.\n\n"
-        f"- **Recall**: Measures the percentage of actual positives correctly identified by the model.\n\n"
-        f"- **F1 Score**: Harmonic mean of precision and recall, balancing both metrics.\n\n"
-        f"- **Support**: Indicates the number of actual occurrences of each class in the dataset."
+        "**Classification Report**\n\n"
+        "- **Precision**: Measures the percentage of true positives out of all "
+        "predicted positives.\n\n"
+        "- **Recall**: Measures the percentage of actual positives correctly "
+        "identified by the model.\n\n"
+        "- **F1 Score**: Harmonic mean of precision and recall, balancing both "
+        "metrics.\n\n"
+        "- **Support**: Indicates the number of actual occurrences of each class "
+        "in the dataset."
     )
 
     st.write("\n")
-    
+
     model_roc_curve = plt.imread(f"outputs/{version}/roccurve.png")
     st.image(model_roc_curve, caption='ROC Curve')
 
     st.write("\n")
 
     st.warning(
-        f"**ROC Curve**\n\n"
-        f"The ROC curve visualizes the model's performance in distinguishing between classes. It plots the true positive rate (TPR) against the false positive rate (FPR). A good model achieves a high TPR with a low FPR."
+        "**ROC Curve**\n\n"
+        "The ROC curve visualizes the model's performance in distinguishing "
+        "between classes. It plots the true positive rate (TPR) against the "
+        "false positive rate (FPR). A good model achieves a high TPR with a low FPR."
     )
 
     st.write("\n")
@@ -75,8 +87,10 @@ def ml_performance_metrics():
     st.write("\n")
 
     st.warning(
-        f"**Confusion Matrix**\n\n"
-        f"A confusion matrix presents a table with predicted vs. actual values. A good model has a high count of true positives (TP) and true negatives (TN), and a low count of false positives (FP) and false negatives (FN)."
+        "**Confusion Matrix**\n\n"
+        "A confusion matrix presents a table with predicted vs. actual values. "
+        "A good model has a high count of true positives (TP) and true negatives "
+        "(TN), and a low count of false positives (FP) and false negatives (FN)."
     )
 
     st.write("\n")
@@ -87,25 +101,29 @@ def ml_performance_metrics():
     st.write("\n")
 
     st.warning(
-        f"**Model Performance (Loss & Accuracy)**\n\n"
-        f"- **Loss**: Measures the error for each prediction. Lower loss values indicate better model performance.\n\n"
-        f"- **Accuracy**: Measures how well the model's predictions match the true labels. Higher accuracy on validation data indicates the model generalizes well to unseen data."
+        "**Model Performance (Loss & Accuracy)**\n\n"
+        "- **Loss**: Measures the error for each prediction. Lower loss values "
+        "indicate better model performance.\n\n"
+        "- **Accuracy**: Measures how well the model's predictions match the "
+        "true labels. Higher accuracy on validation data indicates the model "
+        "generalizes well to unseen data."
     )
 
     st.write("\n")
-    
+
     st.write("### Generalized Performance on Test Set")
 
     st.write("\n")
-    
-    test_evaluation = {'Loss': 0.0322, 'Accuracy': 0.9917} 
+
+    test_evaluation = {'Loss': 0.0322, 'Accuracy': 0.9917}
     st.dataframe(pd.DataFrame(test_evaluation, index=['Metric']))
 
     st.write("---")
 
     st.markdown(
-        f"<div style='text-align: center;'>"
-        f"For further details, refer to the "
-        f"<a href='https://github.com/Edgarkimbugwe/CLMP-Detector/blob/main/README.md'>Project README file</a>."
-        f"</div>",
-    unsafe_allow_html=True)
+        "<div style='text-align: center;'>"
+        "For further details, refer to the "
+        "<a href='https://github.com/Edgarkimbugwe/CLMP-Detector/blob/main/README.md'>"
+        "Project README file</a>.</div>",
+        unsafe_allow_html=True,
+    )
